2071bee455f0d4ca847e15dba2498b71bef57b5a
Mike <mharrisvr@users.noreply.github.com>
2024-04-30 17:06:03 -0400

    Update gp_fixer.ps1

--- gp_fixer.ps1
+++ gp_fixer.ps1
@@ -1,7 +1,7 @@
 # Automagically rebuild group policy 
 # 4/30/2024 
 # Mike Harris
-# Made some changes before uploading.. needs a little more fixing. Not functional*
+# Needs some fiddling still
 
 $Date = Get-Date
 $BadDate = $Date.AddDays(-30)
@@ -10,19 +10,16 @@ $MachinePath = "C:\Windows\System32\GroupPolicy\Machine"
 $UserPath = "C:\Windows\System32\GroupPolicy\User"
 $filename = "registry.pol"
 
-#Test if registry.pol file exists
-
+#Test if registry.pol file exists in machinePolicyPath
 $Exists0 = Test-Path -Path $MachinePath\$filename
 
-$Exists1 = Test-Path
-
-If ($Exists -eq 'True') {
+If ($Exists0 -eq 'True') {
     #Test if file is > X amount of days old 
-    $lastModifiedDate = (Get-Item "$Path\$filename").LastWriteTime
+    $lastModifiedDate = (Get-Item "$MachinePath\$filename").LastWriteTime
 
     If ($lastModifiedDate -lt $BadDate){
         Write-Output "Uh oh, Group policy made an oopsie..Fixing that now"
-        Remove-Item $Path\$filename -Force
+        Remove-Item $MachinePath\$filename -Force
         Gpupdate /force 
     } 
     
@@ -31,6 +28,20 @@ If ($Exists -eq 'True') {
     }
 }
 
+#Verifies User registry.pol 
+$Exists1 = Test-Path -Path $UserPath\$filename
 
-#Update Group Policy to pull new registry.pol
-#gpupdate /force 
+If ($Exists1 -eq 'True') {
+    #Test if file is > X amount of days old 
+    $lastModifiedDate = (Get-Item "$UserPath\$filename").LastWriteTime
+
+    If ($lastModifiedDate -lt $BadDate){
+        Write-Output "Uh oh, Group policy made an oopsie..Fixing that now"
+        Remove-Item $UserPath\$filename -Force
+        Gpupdate /force 
+    } 
+    
+    Else {
+    Write-Output "Group Policy is all set and was last updated on '$lastmodifiedDate'"
+    }
+}
