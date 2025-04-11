5d242ece93845c7b724c89c14fe5627ee6434371
Mike <mharrisvr@users.noreply.github.com>
2025-04-11 14:57:26 -0400

    Create Trigger-AppInstallation.ps1

---
+++ SCCM/Trigger-AppInstallation.ps1
@@ -0,0 +1,37 @@
+# https://timmyit.com/2016/08/08/sccm-and-powershell-force-installuninstall-of-available-software-in-software-center-through-cimwmi-on-a-remote-client/3
+# Worked Wonderfully to push application out to Software center and install on machines that have a client policy that hides the software, apps, and updates tabs. 
+
+Function Trigger-AppInstallation
+{
+ 
+Param
+(
+ [String][Parameter(Mandatory=$True, Position=1)] $Computername,
+ [String][Parameter(Mandatory=$True, Position=2)] $AppName,
+ [ValidateSet("Install","Uninstall")]
+ [String][Parameter(Mandatory=$True, Position=3)] $Method
+)
+ 
+Begin {
+$Application = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object {$_.Name -like $AppName})
+ 
+$Args = @{EnforcePreference = [UINT32] 0
+Id = "$($Application.id)"
+IsMachineTarget = $Application.IsMachineTarget
+IsRebootIfNeeded = $False
+Priority = 'High'
+Revision = "$($Application.Revision)" }
+ 
+}
+ 
+Process
+ 
+{
+ 
+Invoke-CimMethod -Namespace "root\ccm\clientSDK" -ClassName CCM_Application -ComputerName $Computername -MethodName $Method -Arguments $Args
+ 
+}
+ 
+End {}
+ 
+}
