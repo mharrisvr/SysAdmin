cd0ac2d103d0713c3ab023b914f8020e58c23d92
Mike <mharrisvr@users.noreply.github.com>
2025-06-10 16:08:15 -0400

    Update ComputerRandomizer.ps1

--- SCCM/ComputerRandomizer.ps1
+++ SCCM/ComputerRandomizer.ps1
@@ -1,5 +1,5 @@
 # Name: ComputerRandomizer.ps1
-# Purpose: Generate 50 machines from entire SCCM collection for the In-place upgrade test. 
+# Purpose: Generate 50 machines from entire SCCM collection for the In-place upgrade test and adds to new test collection
 # Last updated: 6/10/2025
 
 # Variables
@@ -7,7 +7,8 @@
 $SiteCode = "XXX" # Site code 
 $ProviderMachineName = "Server Name" # SMS Provider machine name
 $CollectionName = "Workstations with Windows 10 v22H2"  # The collection name to pick from
-$NumberToSelect = 50             # Number of random computer names to pick
+$UpdateCollectionName = "Win11 24H2 InPlace Upgrade - 3rd Test Group"
+$NumberToSelect = 5             # Number of random computer names to pick
 
 # Import SCCM module and connect to site
 Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
@@ -21,7 +22,7 @@ if (-not $collection) {
 }
 
 # Get all members of the collection
-$allComputers = Get-CMDevice -CollectionName $CollectionName | Select-Object -ExpandProperty Name
+$allComputers = Get-CMDevice -CollectionName $CollectionName | Select-Object Name, PrimaryUser, ResourceID
 
 # Check if there are enough computers
 if ($allComputers.Count -lt $NumberToSelect) {
@@ -34,6 +35,12 @@ $randomComputers = Get-Random -InputObject $allComputers -Count $NumberToSelect
 
 # Output the results
 $randomComputers | ForEach-Object { Write-Output $_ }
+
+Foreach ($random in $randomComputers) {
+    $ID = $random.ResourceID
+    Add-CMDeviceCollectionDirectMembershipRule -CollectionName $UpdateCollectionName -ResourceId $ID -Verbose -ErrorAction SilentlyContinue
+    Write-Output "Added $random to $UpdateCollectionName"
+}
 
 # Optional: Save to a file
 # $randomComputers | Out-File -FilePath "C:\temp\RandomComputers.txt"
